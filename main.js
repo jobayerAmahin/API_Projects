665c7e251381690b859fb7a6ba72705a4357a73a
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-30 22:27:14 +0300

    Adding new API projects with basic fnctional completion

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
 const people=[
-    {name:'Jaber',job:'nonGov',sallary:40000,id:1,address:{
+    {name:'Jaber',job:'nonGov',sallary:30000,id:1,address:{
         main:'Metsälinnunreitti 2'
     }},
-    {name:'Kaber',job:'nonGov',sallary:40000,id:2,address:{
+    {name:'Kaber',job:'nonGov',sallary:140000,id:2,address:{
         main:'Metsälinnunreitti 2',
         current:'Espoo'
     }},
-    {name:'Aaber',job:'Gov',sallary:40000,id:3,address:{
+    {name:'Aaber',job:'Gov',sallary:20000,id:3,address:{
         main:'Metsälinnunreitti 3',
         current:'Helsinki'
     }},
-    {name:'Maber',job:'nonGov',sallary:40000,id:4,address:{
+    {name:'Maber',job:'nonGov',sallary:50000,id:4,address:{
         main:'Metsälinnunreitti',
         current:'Espoo'
     }},
-    {name:'Saber',job:'Gov',sallary:40000,id:5,address:{
+    {name:'Saber',job:'Gov',sallary:35000,id:5,address:{
         main:'Metsälinnunreitti 4',
        
     }},
-    {name:'Taber',job:'Gov',sallary:40000,id:6,address:{
+    {name:'Taber',job:'Gov',sallary:25000,id:6,address:{
         main:'Metsälinnunreitti 6',
         current:'Vantaa'
     }},
-    {name:'Qaber',job:'nonGov',sallary:40000,id:7},
+    {name:'Qaber',job:'nonGov',sallary:90000,id:7},
 ]
 
 const listParent=document.getElementById('person')
 const showName=(obj)=>{
-    const newList=document.createElement('li')
-    newList.innerText=obj.map((p)=>p.name);
+    obj.map((p)=>{
+        const newList=document.createElement('li')
+        newList.classList.add('cursor-pointer','font-bold','my-4')
+        newList.innerText=p.name;
+        listParent.appendChild(newList);
 
-    listParent.appendChild(newList);
-    console.log(newList)
+        newList.addEventListener('click',()=>{
+            matchId(p.id)
+        })
+    })
+    
 }
+
+const matchId=(id)=>{
+    const getId=people.find((l)=>l.id==id)
+    const personInfo=document.getElementById('info')
+    personInfo.innerHTML=`
+    <div class="card bg-base-100 w-96 shadow-xl">
+     <div class="card-body text-2xl font-bold">
+    
+        <p>Person Job- ${getId.job}</p>
+        <p>Person Sallary- ${getId.sallary}</p>
+        <p>Person Address- ${(getId?.address?.current||"N/A")}</p>
+
+     </div>
+    </div>
+    `
+    console.log(personInfo)
+}
+
 showName(people)
